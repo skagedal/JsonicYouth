143f8a80dff5b6e3f524d088c31294a433247af3
Simon Kågedal Reimer <skagedal@gmail.com>
2017-01-17 19:23:43 +0100

    Initial Commit

---
+++ JsonicYouth/JsonicYouth.h
@@ -0,0 +1,19 @@
+//
+//  JsonicYouth.h
+//  JsonicYouth
+//
+//  Created by Simon Kågedal Reimer on 2017-01-17.
+//  Copyright © 2017 SKR. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JsonicYouth.
+FOUNDATION_EXPORT double JsonicYouthVersionNumber;
+
+//! Project version string for JsonicYouth.
+FOUNDATION_EXPORT const unsigned char JsonicYouthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonicYouth/PublicHeader.h>
+
+
